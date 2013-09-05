06b61a3d298d0f5ab3cce2b07cc360dd91479b7e
Kirby Fike <kirby.fike@pnmac.com>
2013-09-05 10:43:24 -0700

    changes

--- lib/topojson_generator.rb
+++ lib/topojson_generator.rb
@@ -1,27 +1,30 @@
-module TopojsonGenerator
+module TopojsonGeneratorParams
   attr_accessor :state_code
 
   def topojson_with_state_key(state_key = :state_code)
     @state_code = state_key
+    puts "hhhhhhhhhhhhhhhhhhhh"
   end
 
-  def topojson_generator(model_object)
   
-  end
 end
 
-
-
-
+class TopojsonGenerator
+  extend TopojsonGeneratorParams
+  def topojson_generator(model_object)
+    puts "zzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+  end
+end
 
 ## Example
-module Delinquency
-  class DeliquenciesController < ApplicationController
-    extend TopojsonGenerator
-    topojson_with_state_key :state_code
-
-    def index
-      respond_with(topojson_generator(StateState.all))
-    end
-  end
-end
+#module Delinquency
+#  class DeliquenciesController < ApplicationController
+#    extend TopojsonGeneratorParams
+#    topojson_with_state_key :state_code
+#
+#    def index
+#      TopojsonGenerator.topojson_generator(5);
+#      respond_with(topojson_generator(StateState.all))
+#    end
+#  end
+#end
--- topojson_generator.gemspec
+++ topojson_generator.gemspec
@@ -1,13 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'topojson_generator'
   s.version     = '0.0.0'
-  s.date        = '2013-09-7'
+  s.date        = '2013-09-07'
   s.summary     = "A simple gem for inserting Rails model data into a topo json file."
   s.description = "A simple gem for inserting rails model data into a topo json file."
   s.authors     = ["Kirby Fike"]
   s.email       = 'kirbyfike@gmail.com'
   s.files       = ["lib/topojson_generator.rb"]
-  s.homepage    =
-    'http://rubygems.org/gems/topojson_generator'
+  s.homepage    = 'http://rubygems.org/gems/topojson_generator'
   s.license       = 'MIT'
 end
